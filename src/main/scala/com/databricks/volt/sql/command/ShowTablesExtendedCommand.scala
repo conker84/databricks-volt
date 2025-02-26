a6cadfae6ea310b6e7d6ee58342365dbd2c07133
conker84 <santand@gmail.com>
2025-02-26 10:16:05 +0100

    changed show tables column filtering

--- src/main/scala/com/databricks/volt/sql/command/ShowTablesExtendedCommand.scala
+++ src/main/scala/com/databricks/volt/sql/command/ShowTablesExtendedCommand.scala
@@ -39,7 +39,8 @@ object ShowTablesExtendedCommand {
 
   private val nonPushableCols = Set(
     "liquid_clustering_cols",
-    "size"
+    "size",
+    "properties"
   )
 
   private val selectColNames = schema
@@ -55,105 +56,108 @@ case class ShowTablesExtendedCommand(filters: Column)
 
   override def run(spark: SparkSession): Seq[Row] = {
     val baseDf = spark.sql(
-      """
-        |SELECT *
-        |FROM system.information_schema.tables
-        |WHERE table_type NOT IN ('VIEW', 'FOREIGN')
-        |AND table_schema <> 'information_schema'
-        |AND table_catalog NOT IN ('system', '__databricks_internal')
-        |AND data_source_format NOT IN ('UNKNOWN_DATA_SOURCE_FORMAT', 'DELTASHARING')
-        |""".stripMargin)
+        """
+          |SELECT *
+          |FROM system.information_schema.tables
+          |WHERE table_type NOT IN ('VIEW', 'FOREIGN')
+          |AND table_schema <> 'information_schema'
+          |AND table_catalog NOT IN ('system', '__databricks_internal')
+          |AND data_source_format NOT IN ('UNKNOWN_DATA_SOURCE_FORMAT', 'DELTASHARING')
+          |""".stripMargin)
       .select(selectColNames :_*)
 
-    val (list: java.util.List[Row], hasError: Boolean) = try {
-      (baseDf
+    val list: java.util.List[Row] = try {
+      baseDf
         .where(filters)
-        .collectAsList(), false)
+        .collectAsList()
     } catch {
       case _: Throwable =>
-        logWarning(
+        throw new IllegalArgumentException(
           s"""
-            |We cannot push down the filters `$filters` properly.
-            |In order to speed up the query execution please check, and exclude, one of the following:
-            |${nonPushableCols.map(c => s"- `$c`").mkString("\n")}
-            |""".stripMargin)
-        (baseDf.collectAsList(), filters != filterStar)
+             |You can filter only for these columns:
+             |${selectColNames.map(c => s"- `$c`").mkString("\n")}
+             |But you provided the following filtering condition:
+             |$filters
+             |""".stripMargin)
     }
 
     val result: Iterator[Row] = list
       .parallelStream()
-      .map(row => {
-        val dataSourceFormat = row.getAs[String]("data_source_format")
-        val isTableDelta = dataSourceFormat == "DELTA"
-
-        val storagePath = row.getAs[String]("storage_path")
-        val files = ReadFileSystem().read(URI.create(storagePath))
-        val fullSize: Option[Long] = if (files.isEmpty) None else Option(files.map(_.size).sum)
-
-        val (
-          snapshotSize: Option[Long],
-          lcCols: Seq[String],
-          deltaLogSize: Option[Long],
-          props: Map[String, String]
-          ) =
-          if (isTableDelta) {
-            val deltaLogSize = Option(
-              files
-              .filter(_.path.contains("/_delta_log"))
-              .map(_.size)
-              .sum
-            )
-            val (snapshotSize, lcCols, props) = DeltaMetadataUtils.forTable(
-              spark,
-              TableIdentifier(
-                row.getAs[String]("table_name"),
-                Option(row.getAs[String]("table_schema")),
-                Option(row.getAs[String]("table_catalog"))
-              )
-            )
-            (snapshotSize, lcCols, deltaLogSize, props)
-          } else {
-            (None, Seq.empty, None, Map.empty)
-          }
-
-        val size = new GenericRowWithSchema(
-          Array(
-            fullSize.map(_ / toGb).orNull, // full_size_in_gb
-            fullSize.orNull, // full_size_in_bytes
-            snapshotSize.map(_ / toGb).orNull, // last_snapshot_size_in_gb
-            snapshotSize.orNull, // last_snapshot_size_in_bytes
-            deltaLogSize.map(_ / toGb).orNull, // delta_log_size_in_gb
-            deltaLogSize.orNull, // delta_log_size_in_bytes
-          ),
-          sizeSchema)
-        val data: Array[Any] = Array(
-          row.getAs[String]("table_catalog"),
-          row.getAs[String]("table_schema"),
-          row.getAs[String]("table_name"),
-          row.getAs[String]("table_type"),
-          dataSourceFormat, // data_source_format
-          storagePath, // storage_path
-          row.getAs[Timestamp]("created"),
-          row.getAs[String]("created_by"),
-          row.getAs[String]("last_altered_by"),
-          row.getAs[String]("last_altered"),
-          lcCols, // liquid_clustering_cols
-          props, // properties
-          size
-        )
-        new GenericRowWithSchema(data, schema)
-      })
+      .map(row => processRow(spark, row))
       .iterator()
       .asScala
       .asInstanceOf[Iterator[Row]]
 
-    if (hasError) {
-      spark.createDataFrame(result.toList.asJava, schema)
-        .where(filters)
-        .collect()
-        .toSeq
-    } else {
-      result.toSeq
+    result.toSeq
+  }
+
+  private def processRow(spark: SparkSession, row: Row): Row = {
+    try {
+      val dataSourceFormat = row.getAs[String]("data_source_format")
+      val isTableDelta = dataSourceFormat == "DELTA"
+
+      val storagePath = row.getAs[String]("storage_path")
+      val files = ReadFileSystem().read(URI.create(storagePath))
+      val fullSize: Option[Long] = if (files.isEmpty) None else Option(files.map(_.size).sum)
+
+      val (
+        snapshotSize: Option[Long],
+        lcCols: Seq[String],
+        deltaLogSize: Option[Long],
+        props: Map[String, String]
+        ) =
+        if (isTableDelta) {
+          val deltaLogSize = Option(
+            files
+              .filter(_.path.contains("/_delta_log"))
+              .map(_.size)
+              .sum
+          )
+          val (snapshotSize, lcCols, props) = DeltaMetadataUtils.forTable(
+            spark,
+            TableIdentifier(
+              row.getAs[String]("table_name"),
+              Option(row.getAs[String]("table_schema")),
+              Option(row.getAs[String]("table_catalog"))
+            )
+          )
+          (snapshotSize, lcCols, deltaLogSize, props)
+        } else {
+          (None, Seq.empty, None, Map.empty)
+        }
+
+      val size = new GenericRowWithSchema(
+        Array(
+          fullSize.map(_ / toGb).orNull, // full_size_in_gb
+          fullSize.orNull, // full_size_in_bytes
+          snapshotSize.map(_ / toGb).orNull, // last_snapshot_size_in_gb
+          snapshotSize.orNull, // last_snapshot_size_in_bytes
+          deltaLogSize.map(_ / toGb).orNull, // delta_log_size_in_gb
+          deltaLogSize.orNull, // delta_log_size_in_bytes
+        ),
+        sizeSchema)
+      val data: Array[Any] = Array(
+        row.getAs[String]("table_catalog"),
+        row.getAs[String]("table_schema"),
+        row.getAs[String]("table_name"),
+        row.getAs[String]("table_type"),
+        dataSourceFormat, // data_source_format
+        storagePath, // storage_path
+        row.getAs[Timestamp]("created"),
+        row.getAs[String]("created_by"),
+        row.getAs[String]("last_altered_by"),
+        row.getAs[String]("last_altered"),
+        lcCols, // liquid_clustering_cols
+        props, // properties
+        size
+      )
+      new GenericRowWithSchema(data, schema)
+    } catch {
+      case ie: InterruptedException => {
+        Thread.currentThread().interrupt()
+        throw ie
+      }
+      case t: Throwable => throw t
     }
   }
 }
